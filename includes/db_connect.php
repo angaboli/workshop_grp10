36ab38479ef380a8795fef1a18a47b75411bf6f7
Ngabonziza Aimé <nzizaba@gmail.com>
2021-09-29 13:35:54 +0200

    database update

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = "localhost";               // Database Host
-$db_user = "root";                    // Database User
-$db_pass = "";                        // Database Password
-$db_name = "website_visitor_counter"; // Database Name
+$db_user = "workshop";                    // Database User
+$db_pass = "WorkShopGrp10_2021";                        // Database Password
+$db_name = "workshopgrp10"; // Database Name
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name); // Connect to Database
 
